2a1579952881edb452e73a3fc56c2f42f1127cdc
Aggy <muneeaggy@gmail.com>
2024-07-06 19:03:45 +0300

    My commit

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+//Code your solutions in this file
+
+const writeCards= function(names,eventName){
+    let newArray=[];
+    for (let i=0 ; i<names.length ; i++){
+        newArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+    }
+    return newArray
+
+}
+// writeCards(["Agnes","Munee","Staus"],"thank you")
+console.log(writeCards(["Agnes","Munee","Staus"],"surprise"));
+
+const countDown = function(n){
+    while(n>=0){
+        console.log(n--)
+    }
+
+}
+countDown(10);
